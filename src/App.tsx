ce052e3fdafc01047b9dea61cd4414c6d61442c3
Hung Le <88levanhung@gmail.com>
2025-08-15 16:06:38 +0700

    ll

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='flex justify-items-center w-full'>
       <Button type="primary">HUNG</Button>
-      HELLO 123 c
+      HELLO a
       <div className='text-primary bg-amber-300'>HE HE</div>
     </div>
   )
